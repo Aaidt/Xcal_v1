d8f01a8332204588963e555a40589ee876916b4d
Aaidt <aadits2004@gmail.com>
2025-07-22 19:38:40 +0530

    http inint

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"  
+
+const app = express();
+
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL || "http://localhost:3001"
+}));
+app.use(cookieParser())
+
+
+// routes
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
+})
