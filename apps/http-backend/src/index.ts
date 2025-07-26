6877599a1a6b5318d29471cd3619ffdcc81c8b1e
Aaidt <aadits2004@gmail.com>
2025-07-26 12:30:29 +0530

    starting fresh

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -15,6 +15,4 @@ app.use(cookieParser())
 // routes
 
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000")
-})
+app.listen(3000, () => { console.log("Server is listening on port 3000") });
