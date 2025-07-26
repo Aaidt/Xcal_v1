ce38542f4aacab3fa3dc55329ea08fdbfdf8debe
Aaidt <aadits2004@gmail.com>
2025-07-26 14:01:30 +0530

    next-auth

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import { Providers } from "./provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { Appbar } from "../components/AppBar";
+import { NEXT_AUTH_CONFIG } from "../lib/auth";
+import { getServerSession } from "next-auth"
+import { signIn } from "next-auth/react";
+
+async function getUser() {
+  const session = await getServerSession(NEXT_AUTH_CONFIG);
+  return session;
+}
+
+export default async function Home() {
+  const session = await getUser();
+
   return (
     <div>
-     
+      <Appbar />
+      {JSON.stringify(session)}
     </div>
   );
 }
