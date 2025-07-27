dcaad52ab8bc5a133f62e4fcf561b530d0dfad51
Aaidt <aadits2004@gmail.com>
2025-07-27 16:22:37 +0530

    auth flow changed

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,6 @@
 import { WebSocketServer, WebSocket } from "ws";
 import { prismaClient } from "@repo/db/client";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 })
 
@@ -10,10 +11,53 @@ interface User {
 }
 const users: User[] = []
 
-wss.on("connection", function connect(ws) {
-    let authorized: boolean;
+const JWT_SECRET = process.env.JWT_SECRET;
 
+function verify(token: string): string | null {
+
+    if(typeof token !== "string"){
+        console.log("Incorrect jwt token.");
+        return null
+    }
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+        if(!decoded.userId){
+            console.log("No user Id.")
+            return null
+        }
+        return decoded.userId;
+    }catch(err){
+        console.log("Error: " + err);
+        return null;
+    }
+}
+
+
+wss.on("connection", function connect(ws, request) {
     ws.on('error', console.error)
+
+    const url = request.url;
+    if(!url){
+        ws.send("Wrong url.")
+        return
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get('token') ?? "";
+
+    const userId = verify(token);
+    if(userId === null){
+        console.log("Not authorized.")
+        ws.close()
+        return
+    }
+
+    users.push({
+        ws,
+        userId,
+        rooms: []
+    })
+
     ws.send("You are connected to the websocket server.")
 
     ws.on("message", async function message(data) {
@@ -21,76 +65,36 @@ wss.on("connection", function connect(ws) {
         try {
             parsedData = JSON.parse(data.toString());
         } catch (err) {
-            ws.send("Incorrect message format.")
-            console.error("Error: " + err)
+            ws.send("Incorrect message format." + err)
             return
         }
 
-
-        if (parsedData.type === "auth") {
-            const userId = parsedData.userId
-            const slug = parsedData.slug
-
-            try {
-                const user = await prismaClient.room.findFirst({
-                    where: {
-                        slug,
-                        OR: [
-                            { adminId: userId },
-                            { link: { link: parsedData.link } }
-                        ]
-                    }
-                })
-                if (!user) {
-                    ws.send("Not authorized. Given userId doesnt match.");
-                    return;
-                }
-                authorized = true;
-                ws.send("You are authorized");
-
-                users.push({
-                    userId,
-                    ws,
-                    rooms: []
-                });
-            } catch (err) {
-                ws.send("Server error. Closing the connection.");
-                ws.close();
-                console.log("Error: " + err);
-            }
+        const user = users.find(user => user.ws === ws);
+        if (!user) {
+            ws.send("No user found...");
+            ws.close();
+            return
         }
 
-        if (authorized) {
-            const user = users.find(user => user.ws === ws);
-            if(!user){
-                ws.send("No user found...");
-                ws.close();
-                return 
-            }
-
-            if (parsedData.type === "join-room") {
-
-                const link = parsedData.link;
-                try {
-                    const room = await prismaClient.link.findFirst({
-                        where: { link },
-                        select: { roomId: true }
-                    });
-                    if(!room || !room.roomId){
-                        ws.send("Cannot find the room corresponding to this link");
-                        return
-                    }
-
-                    user.rooms.push(room.roomId);
-                } catch (err) {
-                    ws.send("Error in fetching the rooms.")
-                    console.error("Error is: " + err);
+        if (parsedData.type === "join_room") {
+            const link = parsedData.link;
+            try {
+                const room = await prismaClient.link.findFirst({
+                    where: { link },
+                    select: { roomId: true }
+                });
+                if (!room || !room.roomId) {
+                    ws.send("Cannot find the room corresponding to this link");
+                    return
                 }
+
+                user.rooms.push(room.roomId);
+            } catch (err) {
+                ws.send("Error in fetching the rooms.")
+                console.error("Error is: " + err);
             }
         }
     })
-
-
 })
 
 wss.on("listening", () => console.log("Server is listening on port 8080."))
