7138e10a844f10ec74f501f0c99e08f8ec5bc6a3
Aaidt <aadits2004@gmail.com>
2025-07-27 15:19:56 +0530

    joining room logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -8,64 +8,88 @@ interface User {
     ws: WebSocket,
     rooms: string[]
 }
+const users: User[] = []
 
-wss.on("connection", function connect(ws){
+wss.on("connection", function connect(ws) {
     let authorized: boolean;
-    const users: User[] = []
 
     ws.on('error', console.error)
     ws.send("You are connected to the websocket server.")
 
-    ws.on("message", async function message(data){
+    ws.on("message", async function message(data) {
         let parsedData;
-        try{
+        try {
             parsedData = JSON.parse(data.toString());
-        }catch(err){
+        } catch (err) {
             ws.send("Incorrect message format.")
             console.error("Error: " + err)
             return
         }
 
 
-        if(parsedData.type === "auth"){
+        if (parsedData.type === "auth") {
             const userId = parsedData.userId
-            const slug  = parsedData.slug
-            
-            try{
-                const user = await prismaClient.room.findFirst({ 
-                    where: { 
+            const slug = parsedData.slug
+
+            try {
+                const user = await prismaClient.room.findFirst({
+                    where: {
                         slug,
                         OR: [
                             { adminId: userId },
                             { link: { link: parsedData.link } }
                         ]
-                    } 
+                    }
                 })
-                if(!user){
+                if (!user) {
                     ws.send("Not authorized. Given userId doesnt match.");
                     return;
                 }
-            }catch(err){
+                authorized = true;
+                ws.send("You are authorized");
+
+                users.push({
+                    userId,
+                    ws,
+                    rooms: []
+                });
+            } catch (err) {
                 ws.send("Server error. Closing the connection.");
                 ws.close();
                 console.log("Error: " + err);
-            } finally {
-                authorized = true;
-                ws.send("You are authorized");
-                
             }
         }
 
-        if(parsedData.type === "join-room"){
-            if(!authorized){
-                ws.send("You are not authorized.")
+        if (authorized) {
+            const user = users.find(user => user.ws === ws);
+            if(!user){
+                ws.send("No user found...");
                 ws.close();
                 return 
             }
 
-        }
+            if (parsedData.type === "join-room") {
 
+                const link = parsedData.link;
+                try {
+                    const room = await prismaClient.link.findFirst({
+                        where: { link },
+                        select: { roomId: true }
+                    });
+                    if(!room || !room.roomId){
+                        ws.send("Cannot find the room corresponding to this link");
+                        return
+                    }
+
+                    user.rooms.push(room.roomId);
+                } catch (err) {
+                    ws.send("Error in fetching the rooms.")
+                    console.error("Error is: " + err);
+                }
+            }
+        }
     })
+
 
 })
 
